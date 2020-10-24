21145427b9e33327bb4f06831601cb2d9774069a
Jane <janeridebmx@gmail.com>
2020-10-24 22:44:08 +0300

    Add mappers for Tag and Certificate

---
+++ src/main/java/com/epam/esm/dao/mappers/CertificateMapper.java
@@ -0,0 +1,30 @@
+package com.epam.esm.dao.mappers;
+
+import com.epam.esm.entity.GiftCertificate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import static com.epam.esm.util.Fields.*;
+
+public class CertificateMapper implements RowMapper<GiftCertificate> {
+
+    @Override
+    public GiftCertificate mapRow(ResultSet resultSet, int i) throws SQLException {
+        GiftCertificate certificate = new GiftCertificate();
+        certificate.setId(resultSet.getInt(ID));
+        certificate.setName(resultSet.getString(NAME));
+        certificate.setDescription(resultSet.getString(DESCRIPTION));
+        certificate.setPrice(resultSet.getDouble(PRICE));
+        certificate.setLastUpdateDate(new Timestamp(resultSet.getTimestamp(LAST_UPDATE_DATE)
+                .getTime())
+                .toLocalDateTime());
+        certificate.setCreateDate(new Timestamp(resultSet.getTimestamp(CREATE_DATE).getTime())
+                .toLocalDateTime());
+        certificate.setId(resultSet.getInt(ID));
+
+        return certificate;
+    }
+}
---
+++ src/main/java/com/epam/esm/dao/mappers/TagMapper.java
@@ -0,0 +1,21 @@
+package com.epam.esm.dao.mappers;
+
+import com.epam.esm.entity.Tag;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static com.epam.esm.util.Fields.ID;
+import static com.epam.esm.util.Fields.NAME;
+
+public class TagMapper implements RowMapper<Tag> {
+
+    @Override
+    public Tag mapRow(ResultSet resultSet, int i) throws SQLException {
+        Tag tag = new Tag();
+        tag.setId(resultSet.getInt(ID));
+        tag.setName(resultSet.getString(NAME));
+        return tag;
+    }
+}
