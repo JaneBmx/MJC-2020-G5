258290917bf601e972ab7163bb20b2be20cd4fbb
Jane <janeridebmx@gmail.com>
2020-10-27 20:05:50 +0300

    Add TagDAO interface

---
+++ src/main/java/com/epam/esm/dao/TagDAO.java
@@ -0,0 +1,9 @@
+package com.epam.esm.dao;
+
+import com.epam.esm.entity.Tag;
+
+import java.util.List;
+
+public interface TagDAO extends DAOInterface<Tag> {
+    List<Tag> getByCertificateId(int certificateId);
+}
--- src/main/java/com/epam/esm/dao/impl/TagDAOImpl.java
+++ src/main/java/com/epam/esm/dao/impl/TagDAOImpl.java
@@ -1,6 +1,7 @@
 package com.epam.esm.dao.impl;
 
 import com.epam.esm.dao.AbstractDAO;
+import com.epam.esm.dao.TagDAO;
 import com.epam.esm.dao.mappers.TagMapper;
 import com.epam.esm.entity.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +11,13 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.epam.esm.util.Fields.*;
 
 @Repository
-public class TagDAOImpl extends AbstractDAO<Tag> {
+public class TagDAOImpl extends AbstractDAO<Tag> implements TagDAO {
     @Autowired
     public TagDAOImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         super("select * from tag",
@@ -45,16 +47,36 @@ public class TagDAOImpl extends AbstractDAO<Tag> {
         MapSqlParameterSource queryParams = new MapSqlParameterSource();
         String sortField = params.remove(SORT);
         String sortType = params.get(SORT_TYPE) == null ? SORT_ASC : params.remove(SORT_TYPE);
-        String name = params.get(NAME) == null ? "" : params.remove(NAME);
-        query.append(String.format(" WHERE %s LIKE :tag_name ", NAME));
-        queryParams.addValue("tag_name", "%" + name + "%");
-        if (sortField != null) {
-            query.append(String.format(" ORDER BY :%s :%s", SORT, SORT_TYPE));
-            queryParams.addValue(SORT, sortField).addValue(SORT_TYPE, sortType);
+        String name = params.remove(NAME);
+        String id = params.remove(ID);
+        String predicate = " and";
+
+        if (name != null) {
+            query.append(String.format(" WHERE %s = :tag_name ", NAME));
+            queryParams.addValue("tag_name", name);
+        } else predicate = " WHERE";
+
+        if (id != null) {
+            query.append(String.format(predicate + " %s = :id ", ID));
+            queryParams.addValue(ID, id);
         }
+
+        if (sortField != null && sortField.split(" ").length == 1 && sortType.split(" ").length == 1) //primitive sql-injection protection
+            query.append(String.format(" ORDER BY %s %s", sortField, sortType));
+
         query.append(";");
         Map<String, MapSqlParameterSource> map = new HashMap<>();
         map.put(query.toString(), queryParams);
+        System.out.println(query);
         return map;
+    }
+
+    @Override
+    public List<Tag> getByCertificateId(int certificateId) {
+        String query = "SELECT tag.* FROM tag " +
+                "LEFT JOIN gift_certificate2tag ON tag.id = gift_certificate2tag.tag_id " +
+                "WHERE gift_certificate2tag.gift_certificate_id = " + certificateId + ";";
+
+        return this.namedParameterJdbcTemplate.query(query, this.mapper);
     }
 }
